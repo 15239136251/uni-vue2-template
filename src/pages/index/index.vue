22328dc2739c9130937ab4eaa9937f3a500e7e9d
qu.hb@sungeon.com <qu.hb@sungeon.com>
2023-03-23 22:37:22 +0800

    ✨ feat(添加demo):

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,21 +4,39 @@
 		<view>
 			<text class="title">{{title}}</text>
 		</view>
+		<view class="mt-2 w-1--2 bg-gray-400 h-24 p-1">
+			<!-- <uni-countdown :day="1" :hour="1" :minute="12" :second="40"></uni-countdown> -->
+			<uni-easyinput class="uni-mt-5" trim="all" v-model="value" placeholder="请输入内容" @input="input"></uni-easyinput>
+			<button class="bg-blue-300 text-white mt-4" @click="onClick">修改title</button>
+		</view>
+
 	</view>
 </template>
 
 <script>
+	import { mapState, mapMutations } from 'vuex'
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				// title: 'Hello'
+				value: ''
 			}
 		},
 		onLoad() {
 
 		},
+		computed: {
+			...mapState(['title'])
+		},
 		methods: {
-
+			...mapMutations(['setTitle']),
+			input(e) {
+				console.log("🚀 ~ file: index.vue:33 ~ input ~ e:", e)
+			},
+			onClick() {
+				this.setTitle(this.value)
+				this.value = ''
+			}
 		}
 	}
 </script>
